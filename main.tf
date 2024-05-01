792cbb19c5337ecddb691f2a017251e2ee2c02c9
Yaroslav Tsalko <yariktb124@gmail.com>
2024-05-01 19:12:55 +0300

    update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,9 @@
-variable "AWS_ACCESS_KEY_ID" {
-  default = ""
-}
-variable "AWS_SECRET_ACCESS_KEY" {
-  default = ""
-}
+
 provider "aws" {
   region = "eu-central-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+
 }
-terraform {
-  backend "s3" {
-    bucket = "terraform-bucket-rozklad-bot"
-    key    = "terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
+
 module "ecr" {
   source = "./tfmodules/ecr"
 }
