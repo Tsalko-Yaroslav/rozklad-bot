f420c4a985a75f9e1b6fbb6077bf5c504de3540c
Yaroslav Tsalko <yariktb124@gmail.com>
2024-05-01 19:05:17 +0300

    update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
-
+variable "AWS_ACCESS_KEY_ID" {
+  default = ""
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = ""
+}
 provider "aws" {
   region = "eu-central-1"
-
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 terraform {
   backend "s3" {
