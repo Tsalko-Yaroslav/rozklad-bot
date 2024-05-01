0c48d981e82d7f3871b9e1f190ef9b2eed5be437
Yaroslav Tsalko <yariktb124@gmail.com>
2024-05-01 18:31:32 +0300

    terraform dir refactor

---
+++ main.tf
@@ -0,0 +1,18 @@
+backend "s3" {
+  bucket = "terraform-bucket-rozklad-bot"
+  key    = "tfstate"
+  region = "eu-central-1"
+}
+provider "aws" {
+  region = "eu-central-1"
+
+}
+module "ecr" {
+  source = "tfmodules/ecr"
+}
+module "vpc" {
+  source = "tfmodules/vpc"
+}
+module "ecs" {
+  source = "tfmodules/ecs"
+}
