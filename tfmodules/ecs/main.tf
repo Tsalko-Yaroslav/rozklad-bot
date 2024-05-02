ff6cb755f4432254b49f367819e5938f658ef660
Yaroslav Tsalko <yariktb124@gmail.com>
2024-05-02 15:20:16 +0300

    iam terraform + task definitions fix

--- tfmodules/ecs/main.tf
+++ tfmodules/ecs/main.tf
@@ -7,8 +7,26 @@ provider "aws" {
 resource "aws_ecs_cluster" "rozklad-bot-cluster" {
   name = "rozklad-bot-cluster"
 }
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    "Version"               : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect"    : "Allow",
+        "Principal" : {
+          "Service" : "ecs-tasks.amazonaws.com"
+        },
+        "Action"    : "sts:AssumeRole"
+      }
+    ]
+  })
+
+  # Attach policies here to grant permissions for ECR and other necessary actions
+}
 resource "aws_ecs_task_definition" "rozklad-bot-task-definition" {
   family                   = "rozklad-bot-family"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
